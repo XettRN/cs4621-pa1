5d1014c614bdff0209be248437f433c028fda999
Kyle <ttex101@gmail.com>
2021-09-12 14:17:41 -0400

    Add functionality uses Firebase for image pulling

--- app/src/main/java/com/example/pa1/MainActivity.kt
+++ app/src/main/java/com/example/pa1/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,17 +19,29 @@ class MainActivity : AppCompatActivity() {
 
     private fun randImage() {
         val meme: ImageView = findViewById(R.id.imageView)
-        val randInt = (1..5).random()
 
-        val drawable = when (randInt) {
-            1 -> R.drawable.pepsidog
-            2 -> R.drawable.cokedog
-            3 -> R.drawable.canpepsidog
-            4 -> R.drawable.coorsdog
-            else -> R.drawable.drpepperdog
+        //Cloud storage pulls
+        val storage = Firebase.storage.reference
+        val canDog = storage.child("images/canpepsidog.jpg")
+        val cokeDog = storage.child("images/cokedog.jpg")
+        val coorsDog = storage.child("images/coorsdog.jpg")
+        val drDog = storage.child("images/drpepperdog.jpg")
+        val pepsiDog = storage.child("images/pepsidog.jpg")
+
+        //Random Image
+        val randInt = (1..5).random()
+        val randImage = when (randInt) {
+            1 -> canDog
+            2 -> cokeDog
+            3 -> coorsDog
+            4 -> drDog
+            else -> pepsiDog
         }
 
-        meme.setImageResource(drawable)
+        //Display using Glide
+        Glide.with(this)
+            .load(randImage)
+            .into(meme)
         meme.contentDescription = randInt.toString()
     }
 }
