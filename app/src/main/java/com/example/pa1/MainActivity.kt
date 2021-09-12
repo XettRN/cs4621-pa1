7883ec40cadd6a61d28e38cd601c277fbeb3eddc
Kyle <ttex101@gmail.com>
2021-09-12 11:37:41 -0400

    Add memes and interface

--- app/src/main/java/com/example/pa1/MainActivity.kt
+++ app/src/main/java/com/example/pa1/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.pa1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newMeme: Button = findViewById(R.id.button)
+        newMeme.setOnClickListener { randImage() }
+    }
+
+    private fun randImage() {
+        val meme: ImageView = findViewById(R.id.imageView)
+        val randInt = (1..5).random()
+
+        val drawable = when (randInt) {
+            1 -> R.drawable.pepsidog
+            2 -> R.drawable.cokedog
+            3 -> R.drawable.canpepsidog
+            4 -> R.drawable.coorsdog
+            else -> R.drawable.drpepperdog
+        }
+
+        meme.setImageResource(drawable)
+        meme.contentDescription = randInt.toString()
     }
 }
